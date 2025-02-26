bf1d763cf27805464f8b11cba866fa2babb50802
Immaculate <manjory.sjsu@gmail.com>
2025-02-26 01:18:52 -0800

    for vinci4d tree generation code

--- ExhaustiveTrees.py
+++ ExhaustiveTrees.py
@@ -1,8 +1,4 @@
-class TreeNode:
-    def __init__(self,val=0,left=None,right=None):
-        self.val=val
-        self.left=left
-        self.right=right
+
 """
 In this question we will be writing an exhaustive generator to
 generate trees. Trees are allowed to have any number of children
@@ -20,8 +16,64 @@ Show us the generated trees, and count the total number of trees for:
   1) N = 8, M = 5. 
   2) N = 30, M = 3. 
 """
-class ExhaustiveBinaryTreeList:
-    def generateBinaryTreeList(self,n,m):
+from itertools import combinations
+
+class TreeNode:
+    def __init__(self, id):
+        self.id = id
+        self.children = []
+
+    def __repr__(self):
+        if not self.children:
+            return f"({self.id})"
+        return f"({self.id} -> {', '.join(map(str, self.children))})"
+
+# todo: I will focus on these:
+""" 
+1. use a recursive function to generate the trees
+2. I will make sure that during generation the number of leaves dont go beyond m
+3. I will avoid creating redundant or duplicate trees
+"""
+def generate_trees(n, m):
+    def helper(remaining_nodes, max_leaves):
+        if remaining_nodes == 1:
+            yield TreeNode(0)
+            return
+
+        for split in range(1, remaining_nodes):
+            for combo in combinations(range(1, remaining_nodes), split):
+                child_sizes = [combo[0]] + [combo[i] - combo[i-1] for i in range(1, len(combo))] + [remaining_nodes - combo[-1]]
+
+                subtrees = []
+                for size in child_sizes:
+                    subtrees.append(list(helper(size, max_leaves - 1)))
+
+                for subtree_combination in combinations(subtrees, len(subtrees)):
+                    root = TreeNode(0)
+                    root.children = [st[0] for st in subtree_combination]
+                    yield root
+
+    count = 0
+    for tree in helper(n, m):
+        count += 1
+        print(tree)
+
+    print(f"Total number of trees: {count}")
+
+
+# Example usage:
+print("For N = 8, M = 5:")
+generate_trees(8, 5)
+
+# For larger cases like N=30, M=3, you can uncomment the following lines:
+# print("For N = 30, M = 3:")
+# generate_trees(30, 3)
+
+
+
+        
+
+
 
 
 
